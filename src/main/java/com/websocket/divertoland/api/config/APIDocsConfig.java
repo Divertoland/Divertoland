bcc3aa63337e090f8e9fe7caa00a0aa14b4d5fba
HenriBDev <henribdev@gmail.com>
2024-11-30 15:50:27 -0300

    ✨ feat - Adicionando configuração de tipo de ambiente

--- src/main/java/com/websocket/divertoland/api/config/APIDocsConfig.java
+++ src/main/java/com/websocket/divertoland/api/config/APIDocsConfig.java
@@ -1,10 +1,12 @@
 package com.websocket.divertoland.api.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @Configuration
+@Profile({"dev", "test"})
 @OpenAPIDefinition(info = @Info(title = "Divertoland API", version = "1.0.0"))
-public class DocsConfig {}
+public class APIDocsConfig {}
